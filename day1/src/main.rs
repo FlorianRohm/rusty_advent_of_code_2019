de9c3f8e6c8c54a4158e13636dfb3b522a6fe0fa
Florian Rohm <florian.rohm@tngtech.com>
2019-12-07 13:31:36 +0100

    day 1 solved

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,18 +5,31 @@ fn main() {
     let filename = "./day1/resources/input";
     println!("In file {}", filename);
 
-    let fuel_requirements: i32 = fs::read_to_string(filename)
+    let (fuel_requirements_initial, fuel_requirements_total): (i32, i32) = fs::read_to_string(filename)
         .expect("Something went wrong reading the file")
         .lines()
         .map(|line| i32::from_str(line).expect("Parsing error"))
-        .map(get_fuel)
-        .sum();
+        .map(|module_mass| (get_fuel_requirement_for_mass(module_mass), get_complete_fuel_requirement_for_mass(module_mass)))
+        .fold((0, 0), |acc: (i32, i32), next: (i32, i32)| (acc.0 + next.0, acc.1 + next.1));
 
-    println!("Fuel Requirements:\n{}", fuel_requirements);
+    println!("Fuel Requirements initial: {}", fuel_requirements_initial);
+    println!("Fuel Requirements total:   {}", fuel_requirements_total);
 }
 
-fn get_fuel(i: i32) -> i32 {
+fn get_fuel_requirement_for_mass(i: i32) -> i32 {
     i / 3 - 2
+}
+
+fn get_complete_fuel_requirement_for_mass(initial_mass: i32) -> i32 {
+    let mut additional_fuel = get_fuel_requirement_for_mass(initial_mass);
+    let mut fuel_for_fuel = 0;
+
+    while additional_fuel >= 0 {
+        fuel_for_fuel -= -additional_fuel;
+        additional_fuel = get_fuel_requirement_for_mass(additional_fuel)
+    }
+
+    fuel_for_fuel
 }
 
 
@@ -25,10 +38,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_fuel() {
-        assert_eq!(get_fuel(12), 2);
-        assert_eq!(get_fuel(14), 2);
-        assert_eq!(get_fuel(1969), 654);
-        assert_eq!(get_fuel(100756), 33583);
+    fn test_get_fuel_requirement_for_mass() {
+        assert_eq!(get_fuel_requirement_for_mass(12), 2);
+        assert_eq!(get_fuel_requirement_for_mass(14), 2);
+        assert_eq!(get_fuel_requirement_for_mass(1969), 654);
+        assert_eq!(get_fuel_requirement_for_mass(100756), 33583);
+    }
+
+    #[test]
+    fn test_get_complete_fuel_requirement_for_mass() {
+        assert_eq!(get_complete_fuel_requirement_for_mass(1969), 966);
     }
 }
